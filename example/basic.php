cbf9eeb53164d72d048f002e897381ceaf81a95c
kitdev_mint <knsoftwaredev@gmail.com>
2016-04-20 09:27:48 +0700

    update

---
+++ example/basic.php
@@ -0,0 +1,8 @@
+<?php 
+//DEFINE ROOT PATH
+// method 1:
+define('ROOT', dirname(__FILE__));
+// method 2:
+// define('ROOT', __DIR__);
+
+ ?>
---
+++ t3h_courses/cert_php_01_basic/part1/ex_04/ex_04_01.php
@@ -0,0 +1,24 @@
+<?php include "../../config.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ex_04_01</title>
+	<link rel="stylesheet" href="<?php echo ROOT; ?>/assets/css/style.css">
+</head>
+<body>
+	<article class="container">
+		<form action="" method="POST">
+			<header>
+				<h2>diện tích hình chữ nhật</h2>
+			</header>
+			<section>
+				
+			</section>
+			<footer>
+				<input type="submit" value="Tính" name="sbm">
+			</footer>
+		</form>
+	</article>
+</body>
+</html>
